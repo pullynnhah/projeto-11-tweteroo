9e345591174e15722c0ecbec6449febb69ceb012
Paula Rodrigues <paulinhaabro@gmail.com>
2022-09-02 15:27:58 -0300

    implement post tweet route

--- handlers.js
+++ handlers.js
@@ -16,3 +16,25 @@ const postSignUp = (req, res) => {
     res.status(201).send("OK");
   }
 };
+
+const postTweets = (req, res) => {
+  const {body} = req;
+
+  if (!isValidBody(body, ["username", "tweet"])) {
+    res.status(400).send("Todos os campos são obrigatórios!");
+  } else {
+    TWEETS_DB.push({
+      ...req.body,
+      id: TWEETS_DB.length + 1,
+    });
+    res.status(201).send("OK");
+  }
+};
+
+const getTweets = (req, res) => {};
+
+const getTweetsByUsername = (req, res) => {};
+
+const getTweetsByPage = (req, res) => {};
+
+export {postSignUp, postTweets, getTweets, getTweetsByUsername, getTweetsByPage};
